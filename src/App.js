7e51fceda44e6a80adce3058ff90773cbd354d19
amandena <manda.panda91@yahoo.com>
2020-07-02 22:52:30 -0400

    imported all components into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Blog from './components/Blog'
+import Contact from './components/Contact'
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <h1>React Blog</h1>
+      <h1>Amanda M Johnson</h1>
+      <Home/>
+      <About/>
+      <Portfolio/>
+      <Blog/>
+      <Contact/>
     </div>
-  );
+  )
 }
 
 export default App
