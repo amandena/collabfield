d1e79519d04390e5b0a2fef4795f9379ae05c8d6
amandena <manda.panda91@yahoo.com>
2020-07-02 23:03:49 -0400

    imported profile pic in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import image from '../assets/IMG_2052.jpg'
 
 const Home = () => {
   return(
     <div>
-      Home
+      <img src={image} alt="that's me!" width='400'></img>
     </div>
   )
 }
