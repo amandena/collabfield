8dda066d95f7a164a99c3580f577b5e6528961e7
amandena <manda.panda91@yahoo.com>
2020-07-02 23:07:44 -0400

    imported containers into app

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Blog from './components/Blog'
 import Contact from './components/Contact'
+import NavBar from './containers/NavBar'
+import Footer from './containers/Footer'
 
 function App() {
   return(
     <div className="App">
+      <NavBar/>
+      
       <h1>Amanda M Johnson</h1>
+
       <Home/>
       <About/>
       <Portfolio/>
       <Blog/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
