fecb7ba133a04ffbcd78e6a23466237051106e24
amandena <manda.panda91@yahoo.com>
2020-07-04 15:07:23 -0400

    added placeholder navbar links

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import SocialMedia from '../components/SocialMedia'
 
 const NavBar = () => {
   return(
     <div>
-      NavBar
+      <Link to='/'>Home</Link>
+      <Link to='#'>About</Link>
+      <Link to='#'>Portfolio</Link>
+      <Link to='#'>Blog</Link>
+      <Link to='#'>Contact</Link>
       <SocialMedia/>
     </div>
   )
