c65815786da8ff20391ddf148491b7d9578b2084
amandena <manda.panda91@yahoo.com>
2020-07-06 22:26:55 -0400

    added social media links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SocialMedia = () => {
   return(
     <div className='social-media'>
-      SocialMedia
+      <Link to='https://github.com/amandena'>GitHub</Link>
+      <Link to='https://www.linkedin.com/in/amandena/'>LinkedIn</Link>
+      <Link to='#'>Twitter</Link>
     </div>
   )
 }
 
 export default SocialMedia
+
+// links not going to sites
