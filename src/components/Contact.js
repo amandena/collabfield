22516719291a90968159d16b0753ceeb7f9fc025
amandena <manda.panda91@yahoo.com>
2020-07-08 23:39:58 -0400

    built a basic contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,18 @@ import React from 'react'
 const Contact = () => {
   return(
     <div className='contact'>
-      Contact
+      <form>
+        <input type='text' placeholder='Name' />
+        <input type='text' placeholder='Email' />
+
+        <br/><br/>
+
+        <textarea type='text' placeholder='Message' />
+
+        <br/><br/>
+
+        <input type='submit' value='Send' />
+      </form>
     </div>
   )
 }
