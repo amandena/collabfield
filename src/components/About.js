4eeb59a897b25aadf0540956e69cf44dec110ec0
amandena <manda.panda91@yahoo.com>
2020-07-09 23:25:43 -0400

    updated bio

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,13 @@ import React from 'react'
 const About = () => {
   return(
     <div className='about'>
-      <p>I've been a barista, dog walker, fitness influencer, graphic designer, and managed a successful physical therapy company. After falling in love with coding, I now create applications for the web and am looking for my first full time role as a Full Stack Software Engineer.</p>
+      <p>Hi! I'm Amanda.</p>
+
+      <p>I've been a barista, dog walker, fitness influencer, graphic designer, and managed a successful physical therapy company. After falling in love with coding, I now create applications for the web and am looking for a new full time role as a Full Stack Software Engineer.</p>
       
-      <p>I can be found typing away at my favorite coffee shop, walking my dog around the block or you can just drop me a line <strong>here</strong>.</p>
+      <p>I can be found typing away at my favorite coffee shop, walking my dog around the block, or you can just drop me a line <strong>here</strong>.</p>
+
+      <h3>Here is my current tech stack:</h3>
     </div>
   )
 }
