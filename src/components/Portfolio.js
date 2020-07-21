4b7bfc5e7e4ac4d17280b46f9af6fb445036ff55
amandena <manda.panda91@yahoo.com>
2020-07-20 23:04:18 -0400

    created new divs for each project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,9 +6,21 @@ const Portfolio = () => {
     <div className='portfolio'>
       <h2>Here Are My Solo Projects:</h2>
 
-      <Link to='#'>Datescope</Link>
-      <Link to='#'>Costume Party</Link>
-      <Link to='#'>My Workouts</Link>
+      <div className='project'>
+        <h3>Datescope</h3>
+        <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE</Link>
+        <Link to='https://github.com/amandena/datescope-backend'>GitHub BE</Link>
+      </div>
+
+      <div className='project'>
+        <h3>Costume Party</h3>
+        <Link to='https://github.com/amandena/costume-party-js-project'>GitHub</Link>
+      </div>
+
+      <div className='project'>
+        <h3>My Workouts</h3>
+        <Link to='https://github.com/amandena/my-workouts-sinatra-project'>GitHub</Link>
+      </div>
     </div>
   )
 }
