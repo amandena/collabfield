75392de7afc477c7a55745de668941db6e74e640
amandena <manda.panda91@yahoo.com>
2020-07-20 23:38:30 -0400

    added project images and removed h3s

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import datescope from '../assets/portfolio/datescope.png'
+import party from '../assets/portfolio/party.png'
+import workouts from '../assets/portfolio/workouts.png'
 
 const Portfolio = () => {
   return(
@@ -7,18 +10,27 @@ const Portfolio = () => {
       <h2>Here Are My Solo Projects:</h2>
 
       <div className='project'>
-        <h3>Datescope</h3>
-        <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE</Link>
+        <img src={datescope} alt='datescope' width='600' />
+
+        <br/>
+
+        <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE </Link>
         <Link to='https://github.com/amandena/datescope-backend'>GitHub BE</Link>
       </div>
 
       <div className='project'>
-        <h3>Costume Party</h3>
+        <img src={party} alt='costume party' width='600' />
+
+        <br/>
+
         <Link to='https://github.com/amandena/costume-party-js-project'>GitHub</Link>
       </div>
 
       <div className='project'>
-        <h3>My Workouts</h3>
+        <img src={workouts} alt='my workouts' width='600' />
+
+        <br/>
+
         <Link to='https://github.com/amandena/my-workouts-sinatra-project'>GitHub</Link>
       </div>
     </div>
