a31e931027baa4c1a684fe64e4234faaf478ccd2
amandena <manda.panda91@yahoo.com>
2020-07-22 23:11:51 -0400

    added social media images

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import github from '../assets/social-media/white-gh.png'
+import linkedin from '../assets/social-media/linkedin.png'
+import twitter from '../assets/social-media/twitter.png'
 
 const SocialMedia = () => {
   return(
     <div className='social-media'>
-      <Link to='https://github.com/amandena'>GitHub</Link>
-      <Link to='https://www.linkedin.com/in/amandena/'>LinkedIn</Link>
-      <Link to='#'>Twitter</Link>
+      <Link to='https://github.com/amandena'><img src={github} alt="github" width='25'/></Link>
+      <Link to='https://www.linkedin.com/in/amandena/'><img src={linkedin} alt="linkedin" width='25'/></Link>
+      <Link to='https://twitter.com/amandenacodes'><img src={twitter} alt="twitter" width='25'/></Link>
     </div>
   )
 }
