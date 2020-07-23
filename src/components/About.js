9818af0ed75babeff3f110b0e0676f8832f12de3
amandena <manda.panda91@yahoo.com>
2020-07-22 21:49:36 -0400

    added github and jquery images to tech stack

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import adobe from '../assets/skills/adobe-icon.png'
+import github from '../assets/skills/github.png'
 import css from '../assets/skills/css3.png'
 import html from '../assets/skills/html5.png'
+import jquery from '../assets/skills/jquery.png'
 import js from '../assets/skills/js.png'
 import react from '../assets/skills/react-512.webp'
 import redux from '../assets/skills/redux-283024.png'
@@ -22,15 +24,17 @@ const About = () => {
       <h3>Work smarter, not harder</h3>
 
       <div className='tech-stack'>
-      <img src={adobe} alt="adobe creative suite" width='70'/>
-      <img src={css} alt="css" width='65'/>
-      <img src={html} alt="html" width='67'/>
-      <img src={js} alt="javascript" width='70'/><br/>
-      <img src={react} alt="react" width='80'/>
-      <img src={redux} alt="redux" width='70'/>
-      <img src={ruby} alt="ruby" width='75'/>
-      <img src={rails} alt="rails" width='70'/>
-      <img src={sql} alt="sql" width='70'/>
+        <img src={adobe} alt="adobe creative suite" width='70'/>
+        <img src={github} alt="github" width='75'/>
+        <img src={css} alt="css" width='65'/>
+        <img src={html} alt="html" width='67'/>
+        <img src={jquery} alt="jquery" width='80'/>
+        <img src={js} alt="javascript" width='70'/><br/>
+        <img src={react} alt="react" width='80'/>
+        <img src={redux} alt="redux" width='70'/>
+        <img src={ruby} alt="ruby" width='75'/>
+        <img src={rails} alt="rails" width='70'/>
+        <img src={sql} alt="sql" width='70'/>
       </div>
     </div>
   )
