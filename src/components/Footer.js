fe1e8b558e69b69c4fb836397b84ae8a8ce772c5
amandena <manda.panda91@yahoo.com>
2020-07-22 23:11:34 -0400

    put social media in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import SocialMedia from './SocialMedia'
 
 const Footer = () => {
   return(
     <div className='footer'>
+      <SocialMedia/>
       <p>© 2020 Amanda M Johnson</p>
     </div>
   )
