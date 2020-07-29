698b7a634576a8a95d4ef3d95472afdf09b8154b
amandena <manda.panda91@yahoo.com>
2020-07-28 23:37:19 -0400

    converted contact form to a class component and added state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,37 @@
 import React from 'react'
 
-const Contact = () => {
-  return(
-    <div className='contact'>
-      <h2>Contact</h2>
-      <h3>Get in touch</h3>
+class Contact extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  }
 
-      <form>
-        <input type='text' placeholder='Name' />
-
-        <br/>
-
-        <input type='text' placeholder='Email' />
-
-        <br/>
-
-        <textarea type='text' placeholder='Message' />
-
-        <br/>
-
-        <input type='submit' value='Send' />
-      </form>
-    </div>
-  )
+  render() {
+    return(
+      <div className='contact'>
+        <h2>Contact</h2>
+        <h3>Get in touch</h3>
+  
+        <form>
+          <input type='text' placeholder='Name' />
+  
+          <br/>
+  
+          <input type='text' placeholder='Email' />
+  
+          <br/>
+  
+          <textarea type='text' placeholder='Message' />
+  
+          <br/>
+  
+          <input type='submit' value='Send' />
+        </form>
+      </div>
+    )
+  }
+  
 }
 
 export default Contact
