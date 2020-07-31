cd65326d746d7a56be2fce0230b6ee0c72c71f70
amandena <manda.panda91@yahoo.com>
2020-07-30 23:10:04 -0400

    rearranged projects and sizing

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,30 +9,31 @@ const Portfolio = () => {
     <div className='portfolio'>
       <h2>Projects</h2>
       <h3>Code, debug, pet dog. Repeat</h3>
+      <div className='projects'>
+        <div className='project'>
+          <img src={datescope} alt='datescope' width='380'/>
 
-      <div className='project'>
-        <img src={datescope} alt='datescope' width='600' />
+          <br/>
 
-        <br/>
+          <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE </Link>
+          <Link to='https://github.com/amandena/datescope-backend'>GitHub BE</Link>
+        </div>
 
-        <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE </Link>
-        <Link to='https://github.com/amandena/datescope-backend'>GitHub BE</Link>
-      </div>
+        <div className='project'>
+          <img className='party' src={party} alt='costume party' width='350' />
 
-      <div className='project'>
-        <img className='party' src={party} alt='costume party' width='600' />
+          <br/>
 
-        <br/>
+          <Link to='https://github.com/amandena/costume-party-js-project'>GitHub</Link>
+        </div>
 
-        <Link to='https://github.com/amandena/costume-party-js-project'>GitHub</Link>
-      </div>
+        <div className='project'>
+          <img src={workouts} alt='my workouts' width='420' />
 
-      <div className='project'>
-        <img src={workouts} alt='my workouts' width='600' />
+          <br/>
 
-        <br/>
-
-        <Link to='https://github.com/amandena/my-workouts-sinatra-project'>GitHub</Link>
+          <Link to='https://github.com/amandena/my-workouts-sinatra-project'>GitHub</Link>
+        </div>
       </div>
     </div>
   )
