65c602c7a4d5232cb779a79bac34c5e9f1f0ac78
amandena <manda.m.johnson@gmail.com>
2020-08-11 23:44:37 -0400

    fixed links

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
   return(
     <div className='navbar'>
       <Link to='/'>Home</Link>
-      <Link to='#'>About</Link>
-      <Link to='#'>Portfolio</Link>
-      <Link to='#'>Blog</Link>
-      <Link to='#'>Contact</Link>
+      <Link to='/skills'>Skills</Link>
+      <Link to='/portfolio'>Portfolio</Link>
+      <Link to='/blog'>Blog</Link>
+      <Link to='/contact'>Contact</Link>
     </div>
   )
 }
