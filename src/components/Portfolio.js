971a9eba68647d29af4f413b6aa075382071c5b2
amandena <manda.m.johnson@gmail.com>
2020-08-17 22:13:48 -0400

    added another project to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import datescope from '../assets/portfolio/datescope.png'
+import game from '../assets/portfolio/game.png'
 import party from '../assets/portfolio/party.png'
 import workouts from '../assets/portfolio/workouts.png'
 
@@ -17,6 +18,14 @@ const Portfolio = () => {
 
           <Link to='https://github.com/amandena/datescope-frontend'>GitHub FE </Link>
           <Link to='https://github.com/amandena/datescope-backend'>GitHub BE</Link>
+        </div>
+
+        <div className='project'>
+          <img className='game' src={game} alt="amanda's game of life" width='370' />
+
+          <br/>
+
+          <Link to='https://github.com/amandena/2020-04-19-Conways-Game-of-Life-4-day-extended/tree/master/amandas-project'>GitHub</Link>
         </div>
 
         <div className='project'>
