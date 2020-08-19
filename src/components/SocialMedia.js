ce88fba56d33863b0fbdd9aca76af1bb8e7fa267
amandena <manda.m.johnson@gmail.com>
2020-08-18 23:07:10 -0400

    replaced twitter image and link with medium counterparts

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import github from '../assets/social-media/white-gh.png'
 import linkedin from '../assets/social-media/linkedin.png'
-import twitter from '../assets/social-media/twitter.png'
+import medium from '../assets/social-media/medium.png'
 
 const SocialMedia = () => {
   return(
     <div className='social-media'>
       <a href='https://github.com/amandena'><img src={github} alt="github" width='25'/></a>
       <a href='https://www.linkedin.com/in/amandena/'><img src={linkedin} alt="linkedin" width='25'/></a>
-      <a href='https://twitter.com/amandenacodes'><img src={twitter} alt="twitter" width='25'/></a>
+      <a href='https://medium.com/@manda.m.johnson'><img src={medium} className='medium' alt="medium" width='25' /></a>
     </div>
   )
 }
