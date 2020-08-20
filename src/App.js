b1a31315ab411fcac5d218359a3892193381fef2
amandena <manda.m.johnson@gmail.com>
2020-08-19 21:45:30 -0400

    removed Switch and Routes from App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Skills from './components/Skills'
 import Portfolio from './components/Portfolio'
@@ -13,13 +12,7 @@ function App() {
   return(
     <div className='App'>
       <NavBar/>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/skills' component={Skills} />
-        <Route exact path='/portfolio' component={Portfolio} />
-        <Route exact path='/blog' component={Blog} />
-        <Route exact path='/contact' component={Contact} />
-      </Switch>
+      <Home />
       <Skills />
       <Portfolio />
       <Blog />
