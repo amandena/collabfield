7b4d4da2dfda10238dd19b639b3967921f2d9571
amandena <manda.m.johnson@gmail.com>
2020-08-20 21:00:33 -0400

    added react-scroll Links to NavBar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,14 +1,49 @@
 import React from 'react'
-import { Link, animateScroll as scroll } from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const NavBar = () => {
   return(
     <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/skills'>Skills</Link>
-      <Link to='/portfolio'>Portfolio</Link>
-      <Link to='/blog'>Blog</Link>
-      <Link to='/contact'>Contact</Link>
+      <Link 
+        activeClass='active'
+        to='home'
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >Home</Link>
+      <Link 
+        activeClass='active'
+        to='skills'
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >Skills</Link>
+      <Link 
+        activeClass='active'
+        to='portfolio'
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >Portfolio</Link>
+      <Link 
+        activeClass='active'
+        to='blog'
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >Blog</Link>
+      <Link 
+        activeClass='active'
+        to='contact'
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >Contact</Link>
     </div>
   )
 }
