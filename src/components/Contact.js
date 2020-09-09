be63199991a08c8ea87d10cd6e00c0a524efdc49
amandena <manda.m.johnson@gmail.com>
2020-09-08 23:16:49 -0400

    uninstalled 2 packages and removed handleSubmit() code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 
 class Contact extends React.Component {
   state = {
@@ -16,25 +15,6 @@ class Contact extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const message = document.getElementById('message').value
-    axios({
-        method: "POST", 
-        url:"http://localhost:3002/send", 
-        data: {
-            name: name,   
-            email: email,  
-            messsage: message
-        }
-    }).then((response) => {
-        if(response.data.msg === 'success'){
-            alert("Message Sent.") 
-            this.resetForm()
-        } else if(response.data.msg === 'fail'){
-            alert("Message failed to send.")
-        }
-    })
   }
 
   resetForm(){
