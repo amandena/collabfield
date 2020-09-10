754e76a014e38cb34da7c2022044097edf0058bf
amandena <manda.m.johnson@gmail.com>
2020-09-09 22:57:52 -0400

    imported emailjs and now have a working handleSubmit() for contact form that sends emails from site

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 
 class Contact extends React.Component {
   state = {
@@ -15,6 +16,17 @@ class Contact extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault()
+    if(this.state.name !== '' && (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.state.email)) && this.state.message !== '') {
+      emailjs.send('default_service','template_kntisvj', this.state, 'user_ymh1s3CUbm1cv7wsJDyxc')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text)
+        this.resetForm()
+      }, (err) => {
+        console.log('FAILED...', err)
+      })
+    } else {
+      alert('Please fill in all fields and use a valid email address to send a message!')
+    }
   }
 
   resetForm(){
