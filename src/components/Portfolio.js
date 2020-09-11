21b5031ef6a0dd327bd3cf13070538dc5ef6c2aa
amandena <manda.m.johnson@gmail.com>
2020-09-10 23:53:57 -0400

    added project descriptions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ import workouts from '../assets/portfolio/workouts.png'
 const Portfolio = () => {
   return(
     <div className='portfolio'>
-      <h2>Projects</h2>
+      <h2>Portfolio</h2>
       <h3>Code, debug, pet dog. Repeat</h3>
       <div className='projects'>
         <div className='project'>
@@ -17,6 +17,12 @@ const Portfolio = () => {
 
           <a href='https://github.com/amandena/datescope-frontend'>GitHub FE </a>
           <a href='https://github.com/amandena/datescope-backend'>GitHub BE</a>
+
+          <br/>
+
+          <em>Connect on a deeper level to find your true soulmate with matching based on horoscope compatibility.</em>
+
+          <p> This project was built with a single-page web application by using a React/Redux UI & react-router and accomplished RESTful routing with a Rails API which manages the data persistence. I demonstrated use of asynchronous Redux actions with Redux Thunk to send & receive data from a server, & to respond to & modify state change.</p>
         </div>
 
         <div className='project'>
@@ -25,6 +31,12 @@ const Portfolio = () => {
           <br/>
 
           <a href='https://github.com/amandena/2020-04-19-Conways-Game-of-Life-4-day-extended/tree/master/amandas-project'>GitHub</a>
+
+          <br/>
+
+          <em>Recreated Conway’s Game of Life for a solo Hackathon submission.</em>
+
+          <p>For ththis project, I developed a single-page game utilizing React, HTML5, & CSS3 which is maintaining and controlling state flow with the React state object.</p>
         </div>
 
         <div className='project'>
@@ -33,6 +45,12 @@ const Portfolio = () => {
           <br/>
 
           <a href='https://github.com/amandena/costume-party-js-project'>GitHub</a>
+
+          <br/>
+
+          <em>A place to collaborate & plan parties with friends & family during the spookiest time of year.</em>
+
+          <p>With the Costume Party, I created a Rails API using REST API functionality with JavaScript & MVC patterns. Users are provided with encrypted Google authentication by utilizing OAuth.</p>
         </div>
 
         <div className='project'>
@@ -41,6 +59,12 @@ const Portfolio = () => {
           <br/>
 
           <a href='https://github.com/amandena/my-workouts-sinatra-project'>GitHub</a>
+
+          <br/>
+
+          <em>Track all of your workouts in this convenient & easy to use app.</em>
+
+          <p>This is one of my earlier projects where I mapped out a Sinatra database schema and logic utilizing ActiveRecord and SQLite3.</p>
         </div>
       </div>
     </div>
