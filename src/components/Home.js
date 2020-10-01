d45e512f0108ba101b65df2ef91d0eb75d65a8db
amandena <manda.m.johnson@gmail.com>
2020-09-30 23:37:11 -0400

    edited bio in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -73,9 +73,9 @@ const Home = () => {
 
       <h4>Hi! I'm Amanda.</h4>
 
-      <p>I've been a barista, dog walker, fitness influencer, graphic designer, and managed a successful physical therapy company. After falling in love with coding, I now create applications for the web and am looking for a new full time role as a Full Stack Software Engineer. I also recently started a position as a Software Engineer Intern for an early-stage startup.</p>
+      <p>I've been a barista, dog walker, fitness influencer, graphic designer, and managed a successful physical therapy company. After falling in love with coding, I now create applications for the web and am looking for a new full time role as a Full Stack Software Engineer. I recently started a position as a Software Engineer Intern working with a great team at an early-stage startup.</p>
 
-      <p>I graduated from Flatiron School's Full Stack Web Development program this year and also have a Bachelor's degree in Graphic Design from Full Sail University.</p>
+      <p>I graduated from Flatiron School's Full Stack Web Development program earlier this year and have a Bachelor's degree in Graphic Design from Full Sail University.</p>
       
       <p>You can find me walking my dog around the block, or you can just drop me a line <Link 
         activeClass='active'
