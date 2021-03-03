e2ae445d7da4c614b5af8040f8006fd81f94bd56
amandena <manda.m.johnson@gmail.com>
2021-03-02 23:27:20 -0500

    working on getting medium widget functioning again

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
 class Blog extends React.Component {
-  componentDidMount() {
-    window.mediumWidget();
-  }
+  // componentDidMount() {
+  //   window.mediumWidget()
+  // }
   render() {
     return(
       <div className='blog'>
         <h2>Blog</h2>
         <hr/>
-
         <div id="medium-widget"></div>
       </div>
     )
