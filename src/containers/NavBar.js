dce2cd7707092911ffe0de465c01d3fd86575138
amandena <manda.m.johnson@gmail.com>
2021-03-10 23:32:13 -0500

    simplified navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -2,48 +2,30 @@ import React from 'react'
 import { Link } from 'react-scroll'
 
 const NavBar = () => {
-  return(
+
+  const links = [
+    {key: 'home', value: 'Home'},
+    {key: 'skills', value: 'Skills'},
+    {key: 'portfolio', value: 'Portfolio'},
+    {key: 'blog', value: 'Blog'},
+    {key: 'contact', value: 'Contact'}
+  ]
+
+  return (
     <div className='navbar container'>
-      <Link 
-        activeClass='active'
-        to='home'
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >Home</Link>
-      <Link 
-        activeClass='active'
-        to='skills'
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >Skills</Link>
-      <Link 
-        activeClass='active'
-        to='portfolio'
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >Portfolio</Link>
-      <Link 
-        activeClass='active'
-        to='blog'
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >Blog</Link>
-      <Link 
-        activeClass='active'
-        to='contact'
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >Contact</Link>
+      {links.map((link, i) => 
+        <Link 
+          key={i}
+          activeClass='active'
+          to={link.key}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          {link.value}
+        </Link>
+      )}
     </div>
   )
 }
