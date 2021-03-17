c272644edbe4f64ffa5ed9ba5d3932890c7debda
amandena <manda.m.johnson@gmail.com>
2021-03-16 20:18:14 -0400

    bootstrapped blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,9 +8,15 @@ class Blog extends React.Component {
   render() {
     return(
       <div className='blog container'>
-        <h2>Blog</h2>
-        <hr/>
-        <div id="medium-widget"></div>
+        <div className='row'>
+          <div className='col'>
+            <h2>Blog</h2>
+            <hr/>
+          </div>
+          <div className='col'>
+            <div id="medium-widget"></div>
+          </div>
+        </div>
       </div>
     )
   }
